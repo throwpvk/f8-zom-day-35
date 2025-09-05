4e1004bb251db623fca3a98a0aedf7b22e87223f
Phan Van Khai <phanvankhaiofficial@gmail.com>
2025-09-05 22:41:41 +0900

    fix gh repo base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/<f8-zom-day-35>/",
+  base: "/f8-zom-day-35/",
   plugins: [react()],
 });
