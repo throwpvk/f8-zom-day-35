ac2d19f6b1764e79929d2fbe70c486c690a59e4d
throwpvk <phanvankhaiofficial@gmail.com>
2025-09-08 11:55:57 +0900

    fix counter & hashroute

--- src/pages/Counter/index.jsx
+++ src/pages/Counter/index.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import styles from "./Counter.module.scss";
+import { Link } from "react-router-dom";
 
 function Display({ color, fontSize, children }) {
   return (
-    <h1 className={styles.display} style={{ color, fontSize }}>{children}</h1>
+    <h1 className={styles.display} style={{ color, fontSize }}>
+      {children}
+    </h1>
   );
 }
 
-function Button({ children, onClick }) {
+function Button({ children, onClick = null }) {
   return (
     <button className={styles.button} onClick={onClick}>
       {children}
@@ -15,11 +18,13 @@ function Button({ children, onClick }) {
   );
 }
 
-function Nav({ onPlus, onMinus, onReset, onHome }) {
+function Nav({ onPlus, onMinus, onReset }) {
   return (
     <nav className={styles.nav}>
-      <Button onClick={onHome}>
-        <i className="fa-solid fa-house"></i>
+      <Button>
+        <Link to="/">
+          <i className="fa-solid fa-house"></i>
+        </Link>
       </Button>
       <Button onClick={onMinus}>
         <i className="fa-solid fa-minus"></i>
@@ -35,7 +40,7 @@ function Nav({ onPlus, onMinus, onReset, onHome }) {
 }
 
 function Counter() {
-  const texts = ["Chào mừng bạn đến với ứng dụng Counter!"]; 
+  const texts = [];
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(0);
   const [typed, setTyped] = useState("");
@@ -62,18 +67,22 @@ function Counter() {
   return (
     <div className={styles.container}>
       {step < texts.length && (
-        <Display color="cadetblue" fontSize="4vw">{typed}</Display>
+        <Display color="cadetblue" fontSize="4vw">
+          {typed}
+        </Display>
       )}
       {step >= texts.length && (
         <>
-          <Display color={count >= 0 ? "cadetblue" : "chocolate"} fontSize="20vw">
+          <Display
+            color={count >= 0 ? "cadetblue" : "chocolate"}
+            fontSize="20vw"
+          >
             {count}
           </Display>
           <Nav
             onPlus={() => setCount((c) => c + 1)}
             onMinus={() => setCount((c) => c - 1)}
             onReset={() => setCount(0)}
-            onHome={() => (window.location.href = "/")}
           />
         </>
       )}
@@ -82,5 +91,3 @@ function Counter() {
 }
 
 export default Counter;
-
-
