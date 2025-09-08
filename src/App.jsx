178dcbc50f0b242896f497809d4e6597832a606a
throwpvk <phanvankhaiofficial@gmail.com>
2025-09-08 11:42:23 +0900

    Updates

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import AppRoutes from "./components/AppRoutes";
-import "./styles/main.scss";
-function App() {
-  return <AppRoutes />;
-}
-
-export default App;
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/f8-zom-day-35/",
-  plugins: [react()],
-});
